bc812104f1b17cb7495bcc4e108654c73d50877c
Travis Mayberry <mayberry@usna.edu>
2026-08-14 10:10:09 -0400

    Fixed test cases

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -1,13 +1,25 @@
 import { describe, expect, it } from "vitest";
 import { hardwareConfig } from "../src/config/defaultHardware";
-import { STARTER_SKETCH } from "../src/config/starterSketch";
 import { createArduinoInclude } from "../src/emulator/arduinoApi";
 import { runRestrictedJscpp } from "../src/emulator/jscppRuntime";
 import { SimulationEngine, type SimulationEvent } from "../src/emulator/simulationState";
 import { wrapArduinoSource } from "../src/emulator/sourceWrapper";
 
+const BLINK_SKETCH = `int red = 6;
+
+void setup() {
+  pinMode(red, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red, 1);
+  delay(1000);
+  digitalWrite(red, 0);
+  delay(1000);
+}`;
+
 describe("wrapped sketch integration", () => {
-  it("steps the starter loop and produces one-second LED transitions", () => {
+  it("steps a blink loop and produces one-second LED transitions", () => {
     const transitions: Array<{ value: number; time: number }> = [];
     let engine!: SimulationEngine;
     engine = new SimulationEngine(hardwareConfig, undefined, (event: SimulationEvent) => {
@@ -15,7 +27,7 @@ describe("wrapped sketch integration", () => {
         transitions.push({ value: event.value, time: engine.millis() });
       }
     });
-    const wrapped = wrapArduinoSource(STARTER_SKETCH, hardwareConfig);
+    const wrapped = wrapArduinoSource(BLINK_SKETCH, hardwareConfig);
     const debuggerInstance = runRestrictedJscpp(
       wrapped.code,
       createArduinoInclude(engine, () => undefined),
