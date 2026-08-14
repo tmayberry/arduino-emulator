263359cf9b3fcac1f7f996185814536d473a7f74
Travis Mayberry <mayberry@usna.edu>
2026-08-14 10:05:19 -0400

    Made starter sketch blank

--- src/config/starterSketch.ts
+++ src/config/starterSketch.ts
@@ -1,12 +1,6 @@
-export const STARTER_SKETCH = `int red = 6;
-
+export const STARTER_SKETCH = `
 void setup() {
-  pinMode(red, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(red, 1);
-  delay(1000);
-  digitalWrite(red, 0);
-  delay(1000);
 }`;
